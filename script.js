2447666edd1ed80f837081de85562fedd9c001fe
yasmeinelkenany <yasmeinelkenany@gmail.com>
2023-01-06 01:27:13 +0200

    Edit index.html styles.css script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+//styling nav
+
+let navIcon = document.getElementById("nav-icon")
+let navBar = document.getElementById("nav");
+let closeBar = document.getElementById("close-icon")
+
+navIcon.addEventListener("click", function () {
+    navBar.style.cssText = `width: 200px;
+    font-size: 1.2rem`;
+    navIcon.style.display = "none";
+    closeBar.style.display = "inline-block"
+})
+
+closeBar.addEventListener('click', function () {
+    navBar.style.cssText = `width: 0;
+    font-size: 0`;
+    navIcon.style.display = "inline-block";
+    closeBar.style.display = "none"
+})
+
