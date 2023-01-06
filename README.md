3c4470e754fe0565f100dd18eb1f5fb77f286c58
yasmeinelkenany <yasmeinelkenany@gmail.com>
2023-01-06 21:25:06 +0200

    Edit index.html styles.css script.js README.md

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # tic-tac-toe-
+
+styling weapons card
+1- card 0vw, 0vh
+2- when the user open the website it should flow the card to let them choose their weapon
+2- then by clicking the weapon it chould be disapeared again
+3- open the level nav to choose level
+4- after choosing level the board should be appeared
--- script.js
+++ script.js
@@ -1,21 +1,62 @@
 //styling nav
-
 let navIcon = document.getElementById("nav-icon")
 let navBar = document.getElementById("nav");
 let closeBar = document.getElementById("close-icon")
 
-navIcon.addEventListener("click", function () {
+navIcon.addEventListener("click", openBar)
+function openBar() {
     navBar.style.cssText = `width: 100%;
     border-bottom: 2px solid #BBE7FE;
     font-size: 1.2rem`;
     navIcon.style.display = "none";
     closeBar.style.display = "inline-block"
-})
+}
 
-closeBar.addEventListener('click', function () {
+closeBar.addEventListener('click', closeNav);
+function closeNav() {
     navBar.style.cssText = `width: 0;
     font-size: 0`;
     navIcon.style.display = "inline-block";
     closeBar.style.display = "none"
-})
+}
+
+
+
+//styling weapons card 
+let startWindow = document.getElementById("first-window");
+let weaponsCard = document.getElementById("weapons");
+let weapons = document.getElementsByClassName("weapon");
+let userWeapon;
+
+for (const weapon of weapons) {
+    weapon.addEventListener('click', function () {
+        userWeapon = weapon.id;
+        console.log(userWeapon)
+        weaponsCard.style.display = "none"
+        startWindow.style.cssText = ` width: 0`;
+        navBar.click = openBar();
+    })
+}
+
+//levels 
+let levels = document.getElementsByClassName("level");
+let choosenLevel = "easy";
+
+for (const level of levels) {
+    level.addEventListener('click', function () {
+        if (level.id == "chooseWeapon") {
+            startWindow.style.cssText = `width: 100%`;
+            weaponsCard.style.display = "flex"
+        }
+
+        else {
+            choosenLevel = level.id;
+        }
+        navBar.click = closeNav();
+    })
+}
+
+
+
+
 
