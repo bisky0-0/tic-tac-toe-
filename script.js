5c05e0db891076eb2f52da4969a81c7f35771dff
yasmeinelkenany <yasmeinelkenany@gmail.com>
2023-01-11 00:52:01 +0200

    Edit index.html styles.css script.js README.md

--- script.js
+++ script.js
@@ -27,77 +27,135 @@ let startWindow = document.getElementById("first-window");
 let weaponsCard = document.getElementById("weapons");
 let weapons = document.getElementsByClassName("weapon");
 let levels = document.getElementsByClassName("level");
-let choosenLevel = "easy";
 let boardEreas = document.getElementsByClassName("board-areas");
+let choosenLevel = "easy";
+let user;
+let aiPlayer;
+
+/*
+board areas so if the tex content of them was equal the game will anounce the winner
+using html classes rather than typing a lot of script here;
+ */
+let row1 = document.getElementsByClassName("row1")
+let row2 = document.getElementsByClassName("row2")
+let row3 = document.getElementsByClassName("row3")
+let col1 = document.getElementsByClassName("col1")
+let col2 = document.getElementsByClassName("col2")
+let col3 = document.getElementsByClassName("col3")
+let xrow1 = document.getElementsByClassName("xrow1")
+let xrow2 = document.getElementsByClassName("xrow2")
 
 
+//factory function for user and AI or other user 
 function Player(name, scores) {
     scores = 0;
     return { name, scores }
 }
 
-/*I put all logic in the weapon function because the immediatly invoked function
-ake the undefined value of userWeapon before adding event to weapons */
+
+
 for (const weapon of weapons) {
     weapon.addEventListener('click', function () {
-        weaponsCard.style.display = "none"
+        weaponsCard.style.display = "none";
         startWindow.style.cssText = ` width: 0`;
         navBar.click = openBar();
-        let userWeapon = weapon.id;
+        let choosenWeapon = weapon.id;
 
+        user = Player(choosenWeapon, 0);
+        aiPlayer = user.name == "x" ? Player("o", 0) : Player("x", 0)
+    })
+}
 
-        //level choosing 
-        for (const level of levels) {
-            level.addEventListener('click', function () {
-                if (level.id == "chooseWeapon") {
-                    startWindow.style.cssText = `width: 100%`;
-                    weaponsCard.style.display = "flex"
-                }
-
-                else {
-                    choosenLevel = level.id;
-                }
-                navBar.click = closeNav();
-            })
-        }
-
-
-        //after choosing level I start putting logic
-        (function game() {
-            let userPlayer = Player(userWeapon, 0);
-            let PlayerAI;
-            if (userPlayer.name == "x") {
-                PlayerAI = Player("o", 0);
-            }
-            else if (userPlayer.name !== "x") {
-                PlayerAI = Player("x", 0)
-            }
-
-            let areasArray = []
-            for (const area of boardEreas) {
-                areasArray.push(area);
-                if (area.textContent !== "x" || area.textContent !== "o")
-                    area.addEventListener('click', function () {
-                        areasArray.splice(areasArray.indexOf(area), 1);
-                        area.classList.add(userPlayer.name);
-                        area.textContent = userPlayer.name;
-
-
-
-                        let random = Math.floor(Math.random() * areasArray.length);
-                        console.log(areasArray.length)
-                        boardEreas[random].classList.add(PlayerAI.name);
-                        boardEreas[random].textContent = PlayerAI.name;
-                        console.log(random)
-                        areasArray.splice(areasArray.indexOf(random), 1);
-                    })
-
-            }
-        })()
+for (const level of levels) {
+    level.addEventListener("click", function () {
+        navBar.click = closeNav();
+        choosenLevel = level.id;
+        levelFun();
     })
 }
 
 
+
+
+
+let areasArray = [...boardEreas]
+function random() { return Math.floor(Math.random() * areasArray.length) }
+//areas 
+function levelFun() {
+    // let aiTurn = false;
+    for (const area of areasArray) {
+        if (choosenLevel === "easy") {
+            area.addEventListener('click', function () {
+                if (!area.classList.contains("chicked")) {
+                    area.textContent = user.name;
+                    area.classList.add(`${user.name}`, "chicked");
+                    area.style.cssText = `transition: 0s; text-shadow: none`
+                    areasArray.splice(areasArray.indexOf(area), 1);
+                    comparing();
+
+                    //Ai turn
+                    if (areasArray.length > 1) {
+                        let randomNum = random();
+                        areasArray[randomNum].textContent = aiPlayer.name;
+                        console.log(randomNum)
+                        areasArray[randomNum].classList.add(`${aiPlayer.name}`, "chicked")
+                        areasArray[randomNum].style.cssText = `transition: 2s; text-shadow: none`
+                        areasArray.splice(randomNum, 1);
+                        comparing();
+                    }
+                }
+            })
+
+        }
+    }
+}
+
+let resultWindow = document.getElementById("result-window");
+let resultCard = document.getElementById("result-card");
+let winner = document.getElementById("winner");
+let restart = document.getElementById("restart");
+
+function checkTextByClassName(classNameTags) {
+    if (classNameTags[0].textContent !== "" && classNameTags[0].textContent == classNameTags[1].textContent &&
+        classNameTags[1].textContent == classNameTags[2].textContent) {
+        classNameTags[0].style.cssText = `text-shadow: 1px 1px 40px #fff; transition: 3s`
+        classNameTags[1].style.cssText = `text-shadow: 1px 1px 40px #fff; transition: 3s`
+        classNameTags[2].style.cssText = `text-shadow: 1px 1px 40px #fff; transition: 3s`
+        resultWindow.style.cssText = `width: 100%;
+        height: 100%;
+        animation-delay: 4s`;
+        resultCard.style.cssText = `width: 340px;
+        height: 340px;
+        font-size: 2rem`;
+        winner.classList.add(`${classNameTags[0].textContent}`)
+        winner.style.fontSize = '8rem'
+        winner.textContent = `${classNameTags[0].textContent}`
+        restart.style.cssText = `width: 160px; height: 40px; font-size: 1.3rem`
+    }
+}
+
+restart.addEventListener('click', function () {
+    resultWindow.style.cssText = `width: 0;
+    height: 0;
+    animation-delay: 1`;
+    resultCard.style.cssText = `width: 0;
+    height: 0;
+    font-size: 0`;
+    winner.style.fontSize = `0`
+    restart.style.cssText = `width: 0; height: 0; font-size: 0`
+    openBar()
+})
+
+function comparing() {
+    checkTextByClassName(row1)
+    checkTextByClassName(row2)
+    checkTextByClassName(row3)
+    checkTextByClassName(col1)
+    checkTextByClassName(col2)
+    checkTextByClassName(col3)
+    checkTextByClassName(xrow1)
+    checkTextByClassName(xrow2)
+}
 
 
 
