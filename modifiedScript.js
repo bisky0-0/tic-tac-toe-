8e080a888d08cd1e0f738a316202705e520391dc
Biscuit krueger <88213094+bisky0-0@users.noreply.github.com>
2024-05-25 04:33:16 +0300

    Update modifiedScript.js

--- modifiedScript.js
+++ modifiedScript.js
@@ -252,17 +252,6 @@ restart.addEventListener('click', function () {
 });
 
 
-const winingArray = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 4, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [2, 4, 6]
-];
-
 function chasing(array, player) {
     for (let i = 0; i < array.length; i++) {
         if (stopFun === false) {
